d59a58eccc0da49bdd08b3da31688ec82368b33a
Khushi sharma <ks496627@gmail.com>
2024-01-10 13:14:19 +0530

    remove router

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,18 @@ import About from "./About";
 import Service from "./service";
 import Contact from "./contact";
 import Navbar from "./Navbar";
-import { Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Router basename={process.env.REACT_APP_URI}>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home /> }/>
           <Route path="/about" element={<About />} />
           <Route path="/service" element={<Service />} />
           <Route path="/contact" element={<Contact />} />
-
-        </Routes>
-      </Router>
+      </Routes>
     </>
   )
 
